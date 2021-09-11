59d2d8a100ced45f1ce104c4d1fb298944179835
Niloufar_Rahimizadeh <niloufarrahimizade@yahoo.com>
2021-09-11 18:58:20 +0430

    20Shah

--- car.py
+++ car.py
@@ -12,9 +12,17 @@ class Car:
     def read_odometer(self):
         print(f"This car has {self.odometer_reading} miles on it.")
 
+    def update_odometer(self, mileage):
+        self.odometer_reading = mileage
+
 my_new_car = Car('audi', 'a4', 2019)
 
-print(my_new_car.get_descriptive_name())
 
+
+# my_new_car.odometer_reading=15
+# my_new_car.model='Q3'
+# my_new_car.year=2020
+my_new_car.update_odometer(12)
+print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
 
